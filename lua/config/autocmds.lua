388dd1d7009a554e190a0a647b9e564a7b6f3923
Christofer Arleryd <christofer.arleryd@conduktor.io>
2026-01-02 14:49:03 +0000

    Some updates

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -16,3 +16,33 @@
 --     -- end
 --   end,
 -- })
+
+-- Avoid focusing neo-tree when closing other windows
+-- Only triggers when entering a neo-tree buffer, and only for non-floating windows
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "neo-tree *",
+  callback = function()
+    local win = vim.api.nvim_get_current_win()
+    local config = vim.api.nvim_win_get_config(win)
+
+    -- Skip if this is a floating window (intentional focus)
+    if config.relative ~= "" then
+      return
+    end
+
+    -- Find a window with a normal file buffer to focus instead
+    for _, w in ipairs(vim.api.nvim_list_wins()) do
+      local wconfig = vim.api.nvim_win_get_config(w)
+      -- Skip floating windows
+      if wconfig.relative == "" then
+        local b = vim.api.nvim_win_get_buf(w)
+        local bt = vim.bo[b].buftype
+        local ft = vim.bo[b].filetype
+        if bt == "" and ft ~= "neo-tree" then
+          vim.api.nvim_set_current_win(w)
+          return
+        end
+      end
+    end
+  end,
+})
